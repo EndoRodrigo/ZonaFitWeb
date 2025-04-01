9cd5bb312f74872b9f06fdfe8d443b2d9a1d2c9d
EndoRodrigoRodriguez <endorodrigo19@gmail.com>
2025-03-31 21:10:42 -0500

    feat: implementacion de la funcionalidad de agregra nuevo cliente

--- src/main/java/com/endorodrigo/ZonaFitWeb/controller/IndexController.java
+++ src/main/java/com/endorodrigo/ZonaFitWeb/controller/IndexController.java
@@ -3,8 +3,11 @@ package com.endorodrigo.ZonaFitWeb.controller;
 import com.endorodrigo.ZonaFitWeb.modelo.Cliente;
 import com.endorodrigo.ZonaFitWeb.servicio.IClienteServicio;
 import jakarta.annotation.PostConstruct;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
 import jakarta.faces.view.ViewScoped;
 import lombok.Data;
+import org.primefaces.PrimeFaces;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +24,7 @@ public class IndexController {
     @Autowired
     IClienteServicio clienteServicio;
     private List<Cliente> clientes;
+    private Cliente clienteSeleccionado;
     private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
 
 
@@ -32,5 +36,28 @@ public class IndexController {
     public List<Cliente> cargarDatos() {
         clientes = clienteServicio.listarClientes();
         return clientes;
+    }
+
+    public void agregarCliente() {
+        this.clienteSeleccionado = new Cliente();
+    }
+
+    public void guardarCliente() {
+        logger.info("cliente a guardar: " + this.clienteSeleccionado);
+        // Agregar
+        if(this.clienteSeleccionado.getId() == null){
+            this.clienteServicio.guardarCliente(this.clienteSeleccionado);
+            this.clientes.add(this.clienteSeleccionado);
+            logger.info("Ya se creo el Cliente");
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage("Cliente Agregado"));
+        }
+        // Ocultar la ventana modal
+        PrimeFaces.current().executeScript("PF('ventanaModalCliente').hide()");
+        // Actualizar la tabla usando ajax
+        PrimeFaces.current().ajax().update("forma-clientes:mensajes",
+                "forma-clientes:clientes-tabla");
+        // Reset del objeto cliente seleccionado
+        this.clienteSeleccionado = null;
     }
 }
